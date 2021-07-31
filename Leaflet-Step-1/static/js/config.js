715ea361231c74e07330f086460a7b4f19c20d9a
carlosmendozaj <carlos-mendozaj@hotmail.com>
2021-07-31 10:21:19 -0500

    Update

--- Leaflet-Step-1/static/js/config.js
+++ Leaflet-Step-1/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1IjoiY2FybG9zLW1lbmRvemFqIiwiYSI6ImNrcXNveGZ1cDI5N2wydm1teGhwcDVmaHIifQ.7AShvY0raCV9tgR0blqHyA";
--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,9 +1,27 @@
 // Store API
-var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson"
+console.log("It's working")
 
-//Request to URL
+//Request to URL:
 
-d3.json(url).then(function(data) {
+d3.json(queryUrl).then(function(data) {
+    
     createFeatures(data.features);
-});
+  });
+  
+  //Set up colo scheme:
+
+  function circleColor(depth){
+    if (depth >90)
+      return "#cc0000"; 
+    else if (depth >70)
+      return "#ff6600";
+    else if (depth >50)
+      return "#ff9966";
+    else if (depth >30)
+      return "#ffff00";
+    else if (depth >10)
+      return "#33cc33";
+    else return "#00ff00";
+  }
 
